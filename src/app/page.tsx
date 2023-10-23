ccd08e7d69c1ca307ce4e2c9f4b800a44044567d
Mahdi Amooassar <mahdi.amooassar@mehrparsict.com>
2023-10-23 17:15:54 +0330

    add person in list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,235 @@
-import Image from 'next/image'
+'use client'
+//
+//
+//
+//
+//
+// import React, {useState} from 'react';
+// import {useForm} from 'react-hook-form';
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+// function UserForm() {
+//     const [formsCount, setFormsCount] = useState(1);
+//     const {register, handleSubmit, reset} = useForm();
+//     const [users, setUsers] = useState([]);
+//
+//     const onSubmit = (data) => {
+//         setUsers([...users, data]);
+//         reset();
+//     };
+//
+//     const renderForms = () => {
+//         let forms = [];
+//         for (let i = 0; i < formsCount; i++) {
+//             forms.push(
+//                 <form onSubmit={handleSubmit(onSubmit)}>
+//                     <label>نام:</label>
+//                     <input type="text" {...register('firstName', {required: true})} />
+//
+//                     <label>نام خانوادگی:</label>
+//                     <input type="text" {...register('lastName', {required: true})} />
+//
+//                     <label>موبایل:</label>
+//                     <input type="text" {...register('phoneNumber', {required: true})} />
+//
+//                     <button type="submit">افزودن کاربر</button>
+//                 </form>
+//             )
+//             return forms;
+//         }
+//     };
+//
+//     const addForm = () => {
+//         setFormsCount(formsCount + 1);
+//     };
+//
+//     return (
+//         <div>
+//             {renderForms()}
+//             <button onClick={addForm}>افزودن فرم جدید</button>
+//         </div>
+//     )
+// }
+//
+// export default UserForm;
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+//
+//
+//
+// import React, {useState} from "react";
+// // import Select from "react-select"
+// // import Input from "@material-ui/core/Input"
+// interface IFormInput {
+//     firstName: string
+//     lastName: string
+//     iceCreamType: { label: string; value: string }
+// }
+//
+// import { Path, useForm, UseFormRegister, SubmitHandler } from "react-hook-form"
+//
+// interface IFormValues {
+//     "First Name": string
+//     Age: number
+// }
+//
+// type InputProps = {
+//     label: Path<IFormValues>
+//     register: UseFormRegister<IFormValues>
+//     required: boolean
+// }
+//
+// // The following component is an example of your existing Input Component
+// const Input = ({ label, register, required }: InputProps) => (
+//     <>
+//         <label>{label}</label>
+//         <input {...register(label, { required })} />
+//     </>
+// )
+//
+// // you can use React.forwardRef to pass the ref too
+// const Select = React.forwardRef<
+//     HTMLSelectElement,
+//     { label: string } & ReturnType<UseFormRegister<IFormValues>>
+// >(({ onChange, onBlur, name, label }, ref) => (
+//     <>
+//         <label>{label}</label>
+//         <select name={name} ref={ref} onChange={onChange} onBlur={onBlur}>
+//             <option value="20">20</option>
+//             <option value="30">30</option>
+//         </select>
+//     </>
+// ))
+//
+// const App = () => {
+//     const { register, handleSubmit } = useForm<IFormValues>()
+//
+//     const onSubmit: SubmitHandler<IFormValues> = (data) => {
+//         alert(JSON.stringify(data))
+//     }
+//
+//     return (
+//         <form onSubmit={handleSubmit(onSubmit)}>
+//             <Input label="First Name" register={register} required />
+//             <Select label="Age" {...register("Age")} />
+//             <input type="submit" />
+//         </form>
+//     )
+// }
+//
+// export default App
+//
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+// ************************
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+interface Person{
+  name : string,
+  phoneNumber : string,
 }
+
+
+import React from 'react';
+import { useForm, useFieldArray, Controller } from 'react-hook-form';
+import {
+  Grid,
+  Button,
+  TextField,
+  IconButton,
+  Typography,
+} from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
+
+const AddToCard = () => {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Person>();
+
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: 'persons',
+  });
+
+  const onSubmit = (data:Person) => {
+    console.log('data is : ' , data);
+  };
+
+  return (
+      <form onSubmit={handleSubmit(onSubmit)}>
+        {errors && (
+            <div>
+              {Object.keys(errors).map((key) => (
+                  <p key={key} style={{ color: 'red' }}>
+                    {errors[key].message}
+                  </p>
+              ))}
+            </div>
+        )}
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <Typography variant="h6">فرم</Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <Button
+                variant="contained"
+                onClick={() => append({ name: '', phone: '' })}
+            >
+              افزودن فرد جدید
+            </Button>
+          </Grid>
+          {fields.map((field, index) => (
+              <React.Fragment key={field.id}>
+                <Grid item xs={4}>
+                  <Controller
+                      name={`name[${index}]`}
+                      control={control}
+                      rules={{ required: "نام را وارد کنید" }}
+                      render={({ field }) => (
+                          <TextField
+                              {...field}
+                              label={`نام فرد ${index + 1}`}
+                              fullWidth
+                              error={!!errors?.name?.[index]}
+                              helperText={errors?.name?.[index]?.message}
+                          />
+                      )}
+                  />
+                </Grid>
+                <Grid item xs={4}>
+                  <Controller
+                      name={`phone[${index}]`}
+                      control={control}
+                      rules={{
+                        required: "شماره تلفن را وارد کنید",
+                        pattern: { value: /^\d+$/, message: "شماره تلفن معتبر نیست" }
+                      }}
+                      render={({ field }) => (
+                          <TextField
+                              {...field}
+                              label={`شماره تلفن فرد ${index + 1}`}
+                              fullWidth
+                              error={!!errors?.phone?.[index]}
+                              helperText={errors?.phone?.[index]?.message}
+                          />
+                      )}
+                  />
+                </Grid>
+                <Grid item xs={2}>
+                  <IconButton onClick={() => remove(index)}>
+                    <DeleteIcon />
+                  </IconButton>
+                </Grid>
+              </React.Fragment>
+          ))}
+          <Grid item xs={12}>
+            <Button type="submit" variant="contained">
+              ارسال
+            </Button>
+          </Grid>
+        </Grid>
+      </form>
+  );
+};
+
+export default AddToCard;
+
